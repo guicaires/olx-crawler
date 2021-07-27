47e6a1fef5884ddd48adf26390c2cb997f95b06e
Guilherme Caires <pessoalgcaires@gmail.com>
2021-07-27 14:27:15 -0300

    test: checks the search status code

--- tests/Feature/OlxCrawlerTest.php
+++ tests/Feature/OlxCrawlerTest.php
@@ -17,4 +17,18 @@ class OlxCrawlerTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    /**
+     * Checks if the API route returns HTTP status code 200
+     *
+     * @return void
+     */
+    public function test_assert_search_http_status()
+    {
+        $search = "Honda Civic 2020";
+
+        $response = $this->get("/api/olxCrawler/$search");
+
+        $response->assertStatus(200);
+    }
 }
