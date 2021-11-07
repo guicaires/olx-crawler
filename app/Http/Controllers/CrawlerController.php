be9a68b1eb72f0e6d022ee3ff3b7562884b407a6
Guilherme Caires <pessoalgcaires@gmail.com>
2021-11-07 15:28:17 -0300

    fix: error when fetching ad date and time

--- app/Http/Controllers/CrawlerController.php
+++ app/Http/Controllers/CrawlerController.php
@@ -3,11 +3,11 @@
 namespace App\Http\Controllers;
 
 use Goutte\Client;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 
 class CrawlerController extends Controller
 {
+    private $item;
     private $results = array();
     private $url = 'https://olx.com.br/autos-e-pecas/carros-vans-e-utilitarios?q=';
 
@@ -23,14 +23,16 @@ class CrawlerController extends Controller
 
         $page->filter('.fnmrjs-1')->each(function($item, $key) {
 
+            $this->item = $item;
+
             $this->results[$key] = [
-                'title' => $item->filter('h2')->text(),
-                'image' => $item->filter('img')->attr('src'),
-                'description' => $item->filter('.iNpuEh > div:nth-child(3) > span')->text(),
-                'price' => $item->filter('p')->text(),
-                'date' =>  $item->filter('.gHqbSa > div > span:nth-child(1)')->text(),
-                'time' => $item->filter('.gHqbSa > div > span:nth-child(2)')->text(),
-                'location' => $item->filter('.gmtqTp > span')->text(),
+                'title' => $this->getText('h2'),
+                'image' => $this->getImg(),
+                'description' => $this->getText('.iNpuEh > div:nth-child(3) > span'),
+                'price' => $this->getText('p'),
+                'date' =>  $this->getText('.gHqbSa > div > div > span:nth-child(1)'),
+                'time' => $this->getText('.gHqbSa > div > div > span:nth-child(2)'),
+                'location' => $this->getText('.gmtqTp > span'),
             ];
 
         });
@@ -40,5 +42,15 @@ class CrawlerController extends Controller
         }
 
         return response($this->results);
+    }
+
+    private function getText($filter)
+    {
+        return $this->item->filter($filter)->count() ? $this->item->filter($filter)->text() : '';
+    }
+
+    private function getImg($filter = 'img')
+    {
+        return $this->item->filter($filter)->count() ? $this->item->filter($filter)->attr('src') : '';
     }
 }
